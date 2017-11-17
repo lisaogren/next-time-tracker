d72ae6e989e21a5055bf3706ff76e33ae387f3eb
RasCarlito <rascarlito@gmail.com>
2017-11-17 09:23:25 +0100

    Active nav items

--- components/nav/item.js
+++ components/nav/item.js
@@ -1,9 +1,20 @@
 import Link from 'next/link'
+import { withRouter } from 'next/router'
 import { Component } from 'react'
+import classnames from 'classnames'
 
 import Icon from 'components/icon'
 
+@withRouter
 class NavItem extends Component {
+  constructor ({ router, href }) {
+    super()
+
+    this.state = {
+      isActive: router.pathname === href
+    }
+  }
+
   render () {
     const { href = '', onClick = null } = this.props
 
@@ -16,9 +27,12 @@ class NavItem extends Component {
 
   content () {
     const { label = '', onClick = null, icon } = this.props
+    const classes = classnames('navbar-item', {
+      'is-active': this.state.isActive
+    })
 
     return (
-      <a className='navbar-item' onClick={onClick}>
+      <a className={classes} onClick={onClick}>
         {icon ? <Icon name={icon} size='small' /> : ''}
         <span>{label}</span>
         <style jsx>{`
